40dd4be16d756a3f045a516e3b87c484ff4ebf1a
larsyx <gabriele.2000.02@gmail.com>
2023-01-26 22:48:09 +0100

    Correzione bug

--- compileResult/programma.c
+++ compileResult/programma.c
@@ -4,167 +4,27 @@
 #include <math.h>
 #include <stdbool.h>
 
-int  scegliOpzione();
-float  quadrato();
-float  rettangolo();
-float  triangolo();
-float  rombo();
-float  cerchio();
-float  trapezio();
-void main ();
+void main (  int t,int pp, float s, char *size);
+float   f;
 
 
 
-int  scegliOpzione(){
-int   scegli;
- printf(" questo programma calcola l'area di diverse forme geometriche\n");
-printf("scegli tra le seguenti opzioni\n");
-printf("\t0 quadrato\n");
-printf("\t1 rettangolo\n");
-printf("\t2 triangolo\n");
-printf("\t3 rombo\n");
-printf("\t4 cerchio\n");
-printf("\t5 trapezio\n");
-scanf("%d",&scegli);
- if(    scegli<= 5  &&  scegli>= 0    ) {
- return  scegli;
-
-}
-else {
- printf("input errato\n\n");
+void main (  int t,int pp, float s, char *size){
+int   tp =  4 ;
+int   i =  0 ;
+  f= 1.5 ;
+for( int  i =  1 ;  i <  10 ;  i++){
+  f=  s+ f ;
+ tp=  1 - 2  ;
 
 }
  
-return  -1 ;
-
-}
-
-
-float  quadrato(){
-float   l;
- printf("hai scelto quadrato\n");
-printf("inserisci lunghezza lato: ");
-scanf("%f",&l);
- l=pow( l, 2 ) ;
-return  l;
-
-}
-
-
-float  rettangolo(){
-float   h, l;
- printf("hai scelto rettangolo\n");
-printf("inserisci lunghezza lato: \n");
-scanf("%f",&l);
-printf("inserisci lunghezza altezza: \n");
-scanf("%f",&h);
-return   l* h ;
-
-}
-
-
-float  triangolo(){
-float   h, l;
- printf("hai scelto triangolo\n");
-printf("inserisci lunghezza lato: \n");
-scanf("%f",&l);
-printf("inserisci lunghezza altezza: \n");
-scanf("%f",&h);
-return    l* h / 2  ;
-
-}
-
-
-float  rombo(){
-float   d2, d1;
- printf("hai scelto rombo\n");
-printf("inserisci diagonale minore: \n");
-scanf("%f",&d1);
-printf("inserisci diagonale maggiore: \n");
-scanf("%f",&d2);
-return    d1* d2 / 2  ;
-
-}
-
-
-float  cerchio(){
-float   r;
- printf("hai scelto cerchio\n");
-printf("inserisci raggio: \n");
-scanf("%f",&r);
-return  pow( r, 2 ) * 3.14  ;
-
-}
-
-
-float  trapezio(){
-float   h, b2, b1;
- printf("hai scelto trapezio\n");
-printf("inserisci base minore: \n");
-scanf("%f",&b1);
-printf("inserisci base maggiore: \n");
-scanf("%f",&b2);
-printf("inserisci altezza: \n");
-scanf("%f",&h);
-return     b2- b1 * h / 2  ;
-
-}
-
-
-
-void main (){
-int   opz =   scegliOpzione();
-float   risultato =  0.0 ;
-  if(   opz !=  -1  ) {
-  if(   opz== 0   ) {
-  risultato=  quadrato();
-
-}
-else {
-  if(   opz== 1   ) {
-  risultato=  rettangolo();
-
-}
-else {
-  if(   opz== 2   ) {
-  risultato=  triangolo();
-
-}
-else {
-  if(   opz== 3   ) {
-  risultato=  rombo();
-
-}
-else {
-  if(   opz== 4   ) {
-  risultato=  cerchio();
-
-}
-else {
-  if(   opz== 5   ) {
-  risultato=  trapezio();
+ while(   i< 20  ||  tp>- 15    ){
+  i=  i+ 1  ;
+ tp=  i- 2  ;
 
 }
  
-
-}
- 
-
-}
- 
-
-}
- 
-
-}
- 
-
-}
- 
-
-}
- 
-printf("il risultato e': %f\n", risultato);
 return ;
 
 }
